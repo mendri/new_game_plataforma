c092481b2db8ae480357072fad36339b2462b930
mendri <marcalmendrighi@gmail.com>
2025-05-02 18:55:28 -0300

    feat: personagem e sua movimentação inicialmente feitos

---
+++ study-case-platform/scripts/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@export var gravity = 500
+@export var jump_force = 190
+@export var speed = 350
+@export var max_speed = 170
+@export var friction = 10
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	if Input.is_action_pressed("move_right"):
+		velocity.x += speed * delta
+	elif Input.is_action_pressed("move_left"):
+		velocity.x -= speed * delta
+	else:
+		if abs(velocity.x) > 0:
+			velocity.x = int(lerp(velocity.x, 0.0, friction * delta))
+
+	velocity.x = clamp(velocity.x, -max_speed, max_speed)
+	velocity.y += gravity * delta
+
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -jump_force
+
+	move_and_slide()
