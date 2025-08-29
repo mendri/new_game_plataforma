1694d680f41f890a429d4b55bcbf7a594661386e
mendri <marcalmendrighi@gmail.com>
2025-08-29 00:05:23 -0300

    feat: projeto inicial finalizado para apresentacao

--- study-case-platform/scripts/checkpoint.gd
+++ study-case-platform/scripts/checkpoint.gd
@@ -1,9 +1,29 @@
 extends Node2D
 
 @onready var checkpoint_area: Area2D = $CheckpointArea
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-func _on_checkpoint_area_body_entered(body:Node2D) -> void:
+enum State {
+	RESTING,
+	USED,
+	GETTING_BIG,
+}
+
+var current_state = State.RESTING
+
+func _process(_delta: float) -> void:
+	match current_state:
+		State.RESTING:
+			animated_sprite.play("resting")
+		State.USED:
+			animated_sprite.play("used")
+		State.GETTING_BIG:
+			animated_sprite.play("getting_big")
+			print(animated_sprite.frame)
+			if animated_sprite.frame == 9:
+				current_state = State.USED
+
+func _on_checkpoint_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		print("Entrou")
-		print(position)
 		body.set_checkpoint_position(position)
+		current_state = State.GETTING_BIG
